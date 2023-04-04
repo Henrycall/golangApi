205e3dcf6856601a35672626abcbe44ebd7c93f7
Henry Felipe Callera <henry.callera@itlean.com.brr>
2023-04-04 16:14:37 -0300

    add router packege

---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/Henrycall/golangApi/router"
+
+func main()  {
+	router.Initialize()
+}
---
+++ router/router.go
@@ -0,0 +1,12 @@
+package router
+import ("github.com/gin-gonic/gin")
+
+func Initialize(){
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run(":3000") // listen and serve on 0.
+}
