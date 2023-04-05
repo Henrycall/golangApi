8d14ea156f1207072e103cd09ebf72d9b65446e6
Henry Felipe Callera <henry.callera@itlean.com.brr>
2023-04-04 22:51:36 -0300

    create subpackege for routes and functions

--- router/routers.go
+++ router/routers.go
@@ -1,42 +1,16 @@
 package router
 
 import (
+	"github.com/Henrycall/golangApi/handler"
 	"github.com/gin-gonic/gin"
 )
 func initializeRouters(router *gin.Engine){
 	v1 := router.Group("/api/v1")
-	{
-		v1.GET("/opening" , func(ctx *gin.Context){
-			ctx.JSON(200,gin.H{
-				"message" : "GET JOB",
-			})
-
-		})
-		v1.POST("/opening" , func(ctx *gin.Context){
-			ctx.JSON(200,gin.H{
-				"message" : "POST JOB",
-			})
-
-		})
-		v1.PUT("/opening" , func(ctx *gin.Context){
-			ctx.JSON(200,gin.H{
-				"message" : "PUT JOB",
-			})
-
-		})
-		v1.DELETE("/opening" , func(ctx *gin.Context){
-			ctx.JSON(200,gin.H{
-				"message" : "DELETE JOB",
-			})
-
-		})
-		v1.GET("/openings" , func(ctx *gin.Context){
-			ctx.JSON(200,gin.H{
-				"message" : "GET JOB",
-			})
-
-		})
-
-	}
+	
+		v1.GET("/opening" , handler.ShowOpeningHandler)
+		v1.POST("/opening" , handler.CreateOpeningHandler)
+		v1.PUT("/opening" , handler.CreateOpeningHandler)
+		v1.DELETE("/opening" , handler.DeletepeningHandler)
+		v1.GET("/openings" , handler.ListOpeningHandler)
 	
 }
